32ee1fccb83db684b0e68507f5172543a18f37ea
jeffraux <auxcalibur@gmail.com>
2025-12-24 16:22:35 +0800

    WIP - check stores

--- src/components/Product/StoreModal.vue
+++ src/components/Product/StoreModal.vue
@@ -17,11 +17,45 @@ defineProps({
 })
 defineEmits(['close'])
 
+const STATIC_STORES = [
+  {
+    id: 1,
+    name: 'Store A',
+    location: 'New York, NY',
+    availability: 'In stock',
+  },
+  {
+    id: 2,
+    name: 'Store B',
+    location: 'Los Angeles, CA',
+    availability: 'In stock',
+  },
+  {
+    id: 3,
+    name: 'Store C',
+    location: 'Chicago, IL',
+    availability: 'In stock',
+  },
+]
+
+const pinCode = ref('')
+const codeError = ref(false)
 const inputRef = ref<HTMLElement | null>(null)
+const showStores = ref(false)
 
 watch(inputRef, () => {
   inputRef.value?.focus()
 })
+
+const handleEnterPin = () => {
+  if (pinCode.value === '123456') {
+    showStores.value = true
+    codeError.value = false
+  } else {
+    showStores.value = false
+    codeError.value = true
+  }
+}
 </script>
 
 <template>
@@ -33,11 +67,23 @@ watch(inputRef, () => {
             <h3>Check Store Availability</h3>
           </div>
           <div class="modal-body">
-            test input: <input ref="inputRef" name="pin" maxlength="4" />
+            Enter pin code: <input v-model="pinCode" ref="inputRef" name="pin" maxlength="6" />
+            <span v-if="codeError" class="code-error">Code is incorrect!</span>
+            <div class="store-list" v-if="showStores">
+              <div class="store-detail-container" v-for="store in STATIC_STORES" :key="store.id">
+                <div class="store-detail">
+                  <h3>{{ store.name }}</h3>
+                  {{ store.location }}
+                </div>
+                <div class="store-detail store-detail-right">
+                  <span>{{ store.availability }}</span>
+                </div>
+              </div>
+            </div>
           </div>
           <div class="modal-footer">
-            <Button btnLabel="Check" variant="primary" @click="$emit('close')" />
-            <Button btnLabel="Cancel" @click="$emit('close')" />
+            <Button btnLabel="Check" variant="primary" @click="handleEnterPin" />
+            <Button btnLabel="Close" @click="$emit('close')" />
           </div>
         </div>
       </dialog>
@@ -73,7 +119,40 @@ watch(inputRef, () => {
   font-weight: 500;
 }
 .modal-body {
-  margin: 0 0 20px 0;
+  margin: 12px 0 20px 0;
+}
+.modal-body input {
+  width: 100px;
+}
+.code-error {
+  margin-left: 8px;
+  color: red;
+  font-size: 12px;
+}
+.store-list {
+  margin: 8px 0;
+}
+.store-detail-container {
+  display: flex;
+  flex-direction: row;
+  border-bottom: 1px solid #b3b3b3;
+  padding: 8px;
+}
+.store-detail-container:first-child {
+  border-top: 1px solid #b3b3b3;
+}
+.store-detail {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+}
+.store-detail h3 {
+  font-weight: bold;
+  font-size: 14px;
+}
+.store-detail-right {
+  justify-content: flex-end;
+  align-items: flex-end;
 }
 .modal-default-button {
   float: right;
