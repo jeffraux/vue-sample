e881ea79e724c3e78cdff5e4280dcbe6a5047e46
jeffraux <auxcalibur@gmail.com>
2025-12-26 14:07:08 +0800

    unit tests

--- src/views/__tests__/ProductsView.spec.ts
+++ src/views/__tests__/ProductsView.spec.ts
@@ -16,4 +16,30 @@ describe('Products Page', () => {
     const wrapper = mount(ProductsView)
     expect(wrapper.text()).toContain('No products available.')
   })
+  it('shows list of products', async () => {
+    const wrapper = mount(ProductsView)
+    // @ts-expect-error products exists in ProductsView
+    wrapper.vm.products = [
+      {
+        id: 1,
+        title: 'Product A',
+        category: 'A',
+        price: 0,
+      },
+      {
+        id: 2,
+        title: 'Product B',
+        category: 'B',
+        price: 1,
+      },
+      {
+        id: 3,
+        title: 'Product C',
+        category: 'C',
+        price: 2,
+      },
+    ]
+    await wrapper.vm.$nextTick()
+    expect(wrapper.findAll('.product-tile')).toHaveLength(3)
+  })
 })
